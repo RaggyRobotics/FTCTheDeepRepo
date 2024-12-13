8b71e6259b3b4d8c114cc562b3f1cda19fd9a115
RaggyRobotics <r.tuttagunta@harrissutton.org.uk>
2024-12-13 17:07:34 +0000

    Pedro Pathing added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/localization/localizers/DriveEncoderLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/localization/localizers/DriveEncoderLocalizer.java
@@ -38,9 +38,9 @@ public class DriveEncoderLocalizer extends Localizer {
     private Encoder leftRear;
     private Encoder rightRear;
     private double totalHeading;
-    public static double FORWARD_TICKS_TO_INCHES = 1;
-    public static double STRAFE_TICKS_TO_INCHES = 1;
-    public static double TURN_TICKS_TO_RADIANS = 1;
+    public static double FORWARD_TICKS_TO_INCHES = 0.0196;
+    public static double STRAFE_TICKS_TO_INCHES = 0.0131;
+    public static double TURN_TICKS_TO_RADIANS = 0.0016;
     public static double ROBOT_WIDTH = 1;
     public static double ROBOT_LENGTH = 1;
 
